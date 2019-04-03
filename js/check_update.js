22b6b0dd5c61a112935b4f73eaea3a0dd3588880
lily148 <jforum@me.com>
2019-04-03 21:53:41 +0800

    Minor change

--- js/check_update.js
+++ js/check_update.js
@@ -6,12 +6,12 @@ var branch = 'master';
 setTimeout(update, 5000);
 
 function update(){
-	$.ajax({url: 'https://raw.githubusercontent.com/EdddieN/machinon-domoticz_theme/'+ branch +'/theme.json' , async: false, dataType: 'json', success: function(data) {
-		
+	$.ajax({url: 'https://raw.githubusercontent.com/lily148/machinon-domoticz_theme/'+ branch +'/theme.json' , async: false, dataType: 'json', success: function(data) {
+
 		gitVersion = data.version
-			
+
 		if (theme.version !== gitVersion) {
-			newVersionText = 'Machinon version '+ data.version +' '+ language.is_available +'! <a href="https://github.com/EdddieN/machinon-domoticz_theme/tree/' + branch + '" target="_blank">' + language.click_here + '</a>';
+			newVersionText = 'Smart Gateway Theme version '+ data.version +' '+ language.is_available +'! It will be publish together with Smart Gateway Update soon.';
 			notify(newVersionText, 0);
 		}
 	}
